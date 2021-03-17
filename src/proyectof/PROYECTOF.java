a2c9b88ca36eb87710138c9d98eb42e5af660a07
Angel <angelfc.contact@gmail.com>
2021-03-16 18:28:47 -0600

    Commit por angel

--- src/proyectof/PROYECTOF.java
+++ src/proyectof/PROYECTOF.java
@@ -15,7 +15,9 @@ public class PROYECTOF {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+
         System.out.println("Hi");
+        System.out.println("Hi 2");
     }
     
 }
